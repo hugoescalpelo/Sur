7b3cc76a8568256716be81c1f0a8a31da052efbb
Hugo Escalpelo <hugoescalpelo@gmail.com>
2018-06-25 12:33:38 -0500

    Some code added

---
+++ Sur Arduino/Sur_Ver1/Sur_Ver1.ino
@@ -0,0 +1,60 @@
+/*
+ * Ivan Abreu Studio.
+ * 
+ * 15/06/2018
+ * 
+ * This code controls a set of 3 stepper motors, 2 of them are
+ * used to turn the head of the system host, the last one to
+ * turn the needle of a big compass.
+ * 
+ * The target of this pieces is force the host to always see
+ * to south and express it with a big model of a compass.
+ * 
+ * Changelog:
+ * V0.1 First development
+ * 
+ * Team
+ * Iván Abreu Ochoa
+ * Malitzin Cortes
+ * Beto Olguin
+ * Hugo Vargas
+ * 
+ */
+
+//Libraries
+
+//Objects
+
+//Constants
+
+//Variables
+double timeNow (); //Tracks actual time in micros
+
+void setup() 
+{
+  Serial.begin (2000000);//Serial monitor communication
+  Serial.println ("Setting up");
+
+  Serial2.begin (9600);//Bluetooth communication
+  Serial2.println ("BT running on port tx/rx 2);
+
+  setModes ();//Set the pin inputs and outputs
+  setInitialConditions ();
+
+  positionSensorBegin ();
+  testSequence ();
+
+  Serial2.println ("Test sequence finished");
+  Serial.println ("Test sequence finished");
+
+}
+
+void loop() 
+{
+  timeNow = micros (); //Keep track of time instant
+  runAll (); //An asynchronous sequens that checks the bit bang state of every stepper
+  checkSerial ();//Look up for configuration changes and reports to serial monitor
+  checkBluetooth ();//Look up for configuration changes and reports to Bluetooth
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Sur Arduino/Sur_Ver1/pins.ino
@@ -0,0 +1,10 @@
+void setModes ()
+{
+  pinMode (dir1, OUTPUT);
+  pinMode (step1, OUTPUT);
+  pinMode (dir2, OUTPUT);
+  pinMode (step2, OUTPUT);
+  pinMode (dir3, OUTPUT);
+  pinMode (step3, OUTPUT);
+}
+
